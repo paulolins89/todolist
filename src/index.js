2dfaa11a9cfd760fb988ebf68562c3c1a0396492
Paulo Lins <paulolins89@gmail.com>
2021-09-21 05:26:03 -0300

    add tasks to screen, need to make it better

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+import inputNewTask from './taskDOM';
+
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskcontainer = document.getElementById('taskcontainer');
+
+addTaskBtn.addEventListener('click', () =>{
+    taskcontainer.insertBefore(inputNewTask(),addTaskBtn);
+});
--- src/taskDOM.js
+++ src/taskDOM.js
@@ -1,5 +1,20 @@
-const addTaskBtn = document.getElementById('addTaskBtn');
-const activetasks = document.getElementsByClassName('activetasks');
-addTaskBtn.addEventListener('click', () =>{
-    addTaskBtn.insertBefore
-});
+export default function inputNewTask (){
+    let taskForm = document.createElement('div');
+
+    let taskName = document.createElement('input');
+    taskName.setAttribute('type', 'text');
+    let dueDate = document.createElement('input')
+    dueDate.setAttribute('type', 'date');
+    let taskDescription = document.createElement('input');
+    taskDescription.setAttribute('type', 'text');
+    let taskPriority = document.createElement('input');
+    taskPriority.setAttribute('type', 'button');
+
+    taskForm.appendChild(taskName);
+    taskForm.appendChild(dueDate);
+    taskForm.appendChild(taskDescription);
+    taskForm.appendChild(taskPriority);
+
+    return taskForm;
+}
+
